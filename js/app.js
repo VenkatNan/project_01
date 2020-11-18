2808fc88624b1a2ac88c947749f648f27b7ba5b5
VenkatNan <nanditavenkat@gmail.com>
2020-11-18 10:34:42 -0600

    table somewhat renders

--- js/app.js
+++ js/app.js
@@ -1,19 +1,23 @@
 /*-----------------------Constant/ Variabls------------------------------*/
 const baseURL ='https://www.dnd5eapi.co/api/'
 
-let options = document.querySelectorAll('option')
+let options = document.querySelectorAll('select option')
+// console.log(options);
+// options.forEach((opt)=>{
+    
+// });
 /*---------------------------Element Refrences (cached)--------------------------*/
 
 /*-----------------------Event Listners------------------------------*/
-// options.addEventListener('click', (opt)=>{
-//    console.log(opt.selectedIndex.value) 
-//     }
+
+    
+
 
 /*-------------------------Functions----------------------------*/
 function getAllSpells(){
     fetch(baseURL+'spells')
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => renderTable(data));
 }
 function getClassSpells(idx){
     fetch(baseURL+`classes/${idx}/spells`)
@@ -27,9 +31,23 @@ function getSchoolSpells(idx){
         .then(data => console.log(data));
 }
 
+function renderTable(data){
+    data = data.results.flat()
+    data.forEach((spell) => {
+       let newRow = document.createElement('tr')
+       let nameCell = document.createElement('td')
+       nameCell.innerHTML = `${spell.name}`
 
-// function classSelector(){
+        
+
+       document.querySelector('tbody').appendChild(newRow.appendChild(nameCell))
+    });
     
-// }
 
-getSchoolSpells('Evocation');
+}
+
+function showClick(e){
+    console.log(e);
+}
+
+getAllSpells();
