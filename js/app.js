4995ef3939bceca6ec3689c957dbd66f98d232f7
VenkatNan <nanditavenkat@gmail.com>
2020-11-18 09:11:15 -0600

    fetch functions created

--- js/app.js
+++ js/app.js
@@ -2,21 +2,34 @@
 const baseURL ='https://www.dnd5eapi.co/api/'
 
 let options = document.querySelectorAll('option')
-
 /*---------------------------Element Refrences (cached)--------------------------*/
 
 /*-----------------------Event Listners------------------------------*/
-options.addEventListener('click', (opt)=>{
-   console.log(opt.selectedIndex.value) 
-    }
-)
+// options.addEventListener('click', (opt)=>{
+//    console.log(opt.selectedIndex.value) 
+//     }
+
 /*-------------------------Functions----------------------------*/
-function getSpells(idx){
-    fetch(baseURL + +'spells')
+function getAllSpells(){
+    fetch(baseURL+'spells')
     .then(response => response.json())
     .then(data => console.log(data));
 }
-
-function classSelector(){
-    
+function getClassSpells(idx){
+    fetch(baseURL+`classes/${idx}/spells`)
+        .then(response => response.json())
+        .then(data => console.log(data));
 }
+
+function getSchoolSpells(idx){
+    fetch(baseURL+`/spells?school=${idx}`)
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
+
+
+// function classSelector(){
+    
+// }
+
+getSchoolSpells('Evocation');
